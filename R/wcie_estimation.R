faa033d2ae60cf637d62927a936c102944ccedd1
Mathéo Le Floch <lefloch_matheo@yahoo.com>
2025-04-25 14:28:59 +0200

    roxygene à finir

--- R/wcie_estimation.R
+++ R/wcie_estimation.R
@@ -34,8 +34,9 @@ WCIE_estimation <- function(mexpo,var.time, timerange, step,
   time_seq <- seq(from=timerange_min,to=timerange_max,by=step) # sequence de mesure dans la fenêtre choisis
 
   nb_ind <- mexpo$ns  # nb d'individu
-  new_data <- data.frame(id= rep(seq(1,nb_ind),
-                                 each = length(time_seq))
+  id_seq<-mexpo$pprob[[mexpo$call[[4]]]] # séquence d'identifiant utilisée dans le modèle (en vecteur)
+
+  new_data <- data.frame(id= rep(id_seq, each = length(time_seq)) # reprendre la séquence d'ID données par les data de l'individu
   )
 
   # renommer l'identifiant comme celui de l'utilisateur
@@ -54,14 +55,8 @@ WCIE_estimation <- function(mexpo,var.time, timerange, step,
   # utiliser comme fonction
 
   # recompile les effets aléatoires pour la nouvelle fenêtre de données (pour n'importe quelle faction du temps ou pas)
-  var_RE2 <- model.matrix(as.formula(paste("~", mexpo$call[[3]][2])), data = new_data)
 
-  for (h in 1:(ncol(var_RE2)-1)) {
-    colnames(var_RE2)[h+1] <- paste0("var_RE",h)
-  }
-  var_RE1 <- var_RE2[,-1]
-
-  new_data <- cbind(new_data, var_RE1)
+  variable_RE <- model.matrix(as.formula(paste("~", mexpo$call[[3]][2])), data = new_data)
 
   # recup covariable utilisé dans le modèle
   covar<-mexpo$Xnames2[!mexpo$Xnames2 %in% var.time][-1]
@@ -78,39 +73,37 @@ WCIE_estimation <- function(mexpo,var.time, timerange, step,
 
   ######### +  les predRE si il y  a des effets aléatoire dans le modèle ############
 
-  # si la deuxième ligne à le même nom alors renommer la deuxième (uniquement lorsque il y seulement la variable de temps prise en compte)
-  if(colnames(mexpo$predRE)[ncol(mexpo$predRE)]==var.time){
-    colnames(mexpo$predRE)[ncol(mexpo$predRE)]<- "Alea_effect"
+  # récupérer les paramêtres des effets aléatoires du model et les renommer (prendre en compte la présence d'un intercept aléatoire)
+  predRE <- mexpo$predRE
+  if((grepl("^-1", as.character(mexpo$call$random)[2])==F)){
+    variable_RE <- variable_RE[,-1] # enlever l'intercept du varRE généré par le model.matrix
+    for (h in 1:(ncol(predRE)-2)) {
+      colnames(predRE)[h+2] <- paste0("predRE",h)
+    }
+  }else{ # si pas d'intercept aléatoire
+    for (h in 1:(ncol(predRE)-1)) {
+      colnames(predRE)[h+1] <- paste0("predRE",h)
+    }
   }
+  new_data <- merge(new_data, predRE,by =  mexpo$call[[4]]) #merge les predRE (effets aléatoires) par individu
 
-  # récupérer les paramêtres du model
-  new_data <- merge(new_data,mexpo$predRE,by = mexpo$call[[4]])
-
-  new_data$pred_final <- 0
   # si présence d'un intercept aléatoire alors le rajouter dans le calcul de la prédiction
   if(grepl("^-1", as.character(mexpo$call$random)[2])==F){
-    new_data$pred_final <- new_data$Ypred + new_data$intercept
-    # renommer les predRE
-    new_mexpopredRE <- mexpo$predRE[-2]
-    for (g in 1:(ncol(new_mexpopredRE)-1)) {
-      colnames(new_mexpopredRE)[g+1] <- paste0("varRE_model",g)
-    }
-    new_data <- merge(new_data,new_mexpopredRE,by = mexpo$call[[4]])
-    name_se<- "varRE_model"
-  }else{
-    new_mexpopredRE <- mexpo$predRE
-    for (g in 1:(ncol(new_mexpopredRE)-1)) {
-      colnames(new_mexpopredRE)[g+1] <- paste0("varRE_model",g)
-    }
-    new_data <- merge(new_data,new_mexpopredRE,by = mexpo$call[[4]])
-    name_se<- "varRE_model"
+    new_data$Ypred <- new_data$Ypred + new_data$intercept
   }
 
-  # predictY + effet aléatoire
-  for (m in 1:(ncol(var_RE2)-1)) {
-    new_data$pred_final <- new_data$pred_final +
-      (new_data[paste0("var_RE",m)]*new_data[paste0(name_se,m)])
+  # predictY + effet aléatoire*variableRE
+  # si au moins 2 colonnes alors faire la boucle sinon pas de boucle (else)
+  if(is.vector(variable_RE)==F) {
+    for (m in 1:(ncol(variable_RE))) {
+      new_data$Ypred <- new_data$Ypred +
+        (variable_RE[,m]*new_data[paste0("predRE",m)])
+    }
+  }else{
+    new_data$Ypred <- new_data$Ypred +
+      (variable_RE*new_data["predRE1"])
   }
+
   ################################################################################################################
 
 
@@ -120,9 +113,6 @@ WCIE_estimation <- function(mexpo,var.time, timerange, step,
   ###### 2) Récupérer l'historique d'exposition ############
   ##########################################################
   data_expo_pred <- new_data
-  data_expo_pred <- data_expo_pred %>%     #remettre dans l'ordre le  temps par individu
-    arrange(.data[[mexpo$call[[4]]]],.data[[var.time]])
-
 
   if (weightbasis=="NS") {
 
@@ -143,23 +133,20 @@ WCIE_estimation <- function(mexpo,var.time, timerange, step,
 
     ## faire la prédiction * les nouveaux splines (Xi(Tu)*Bk(Tu))
     for (r in 1:length(colnames(B2K))) {
-      data_expo_pred[paste0("COCO",r)] <- data_expo_pred$pred_final * data_expo_pred[colnames(B2K)[r]]
+      data_expo_pred[paste0("COCO",r)] <- data_expo_pred$Ypred * data_expo_pred[colnames(B2K)[r]]
     } #Xi fois les Bk
 
     # somme cummulé pondéré des expositions (sum(Xi*Bk)=Fki)
-    data_cum <- data_expo_pred[mexpo$call[[4]]]
+    data_cum <- unique(data_expo_pred[mexpo$call[[4]]])
     for (r in 1:length(colnames(B2K))) {
-
       WCIE<-aggregate(data_expo_pred[[paste0("COCO", r)]] ~ data_expo_pred[[mexpo$call[[4]]]],
-                      data = data_expo_pred, FUN = sum) #
-      colnames(WCIE)<-c(mexpo$call[[4]],paste0("WCIE", r))
-
-      data_cum <-  merge(data_cum,WCIE,by = mexpo$call[[4]])
+                      data = data_expo_pred, FUN = sum)
+      colnames(WCIE)<-c(mexpo$call[[4]],paste0("WCIE", r)) #renommer les colonnes car aggregate ne renomme pas
+      data_cum <-  cbind(data_cum,WCIE[paste0("WCIE", r)])
     }
-    data_cum <- unique(data_cum)
-
   }
   if(weightbasis=="PS"){
+
     #to be develop
   }
   if(weightbasis=="BS"){
@@ -172,13 +159,12 @@ WCIE_estimation <- function(mexpo,var.time, timerange, step,
   ##############################################
 
   if (reg.type=="logistic"){
-    new_outcome_date <- merge(data, data_cum, by=mexpo$call[[4]])
+    new_outcome_date <- merge(data, data_cum, by=mexpo$call[[4]]) #récupère uniquement les individus utilisés dans le modèle d'exposition
 
     # remplacer les expo par les variables d'exposition dans la formule
     new_expo<-c(NULL)
-    for(p in 1:length(colnames(B2K))) {
-      new_expo <- paste(c(new_expo, paste0("WCIE",p)), collapse = "+") ## donne "ns1+ns2+ns3+ns4"
-    }
+    new_expo <- paste(paste0("WCIE", seq_len(ncol(B2K))), collapse = "+") ## donne "ns1+ns2+ns3+ns4"
+
     formdroite <- as.character(model[3]) ## la partie à droite du tilde
     formdroitebis <- gsub("\\bWCIE\\b", paste("(", new_expo, ")"), formdroite) # remplace "expo", par "(ns1+ns2+ns3+ns4)"
 
@@ -189,7 +175,11 @@ WCIE_estimation <- function(mexpo,var.time, timerange, step,
   if (reg.type=="cox"){
 
   }
-  return(list(model=model_outcome,data=data_expo_pred,mexpo=mexpo,call=new_formula))
+  return(list(model=model_outcome,data=new_data[,1:6],mexpo=mexpo,
+              call=new_formula,splines.quantiles=quantiles,
+              boundary.quantiles=c(b5,b95)
+              )
+         )
 }
 
 
